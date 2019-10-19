2f07060e9851bf005267b7ce58e78e0e82d6d8b9
igenovese <ignacio.genovese@gmail.com>
2019-10-18 17:02:43 -0700

    Working

--- app/src/main/java/ece558/rgenovese/basiccalculator/MainActivity.java
+++ app/src/main/java/ece558/rgenovese/basiccalculator/MainActivity.java
@@ -26,11 +26,17 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView mResult;
 
+    /**
+     * Override onCreate method, called when the activity is
+     * started.
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Get every Button by id
         mPlusButton = (Button) findViewById(R.id.button_op_plus);
         mMinusButton= (Button) findViewById(R.id.button_op_minus);
         mMultButton = (Button) findViewById(R.id.button_op_mult);
@@ -38,15 +44,20 @@ public class MainActivity extends AppCompatActivity {
         mPctButton  = (Button) findViewById(R.id.button_op_pct);
         mSqrtButton = (Button) findViewById(R.id.button_op_sqrt);
 
+        //Get Operands EditText by id
         mOp1        = (EditText) findViewById(R.id.editText_op1);
         mOp2        = (EditText) findViewById(R.id.editText_op2);
 
+        //Get Result TextView by id
         mResult     = (TextView) findViewById(R.id.textView_result);
 
 
+        //For every button, create an onClick listener, check operands and if they're
+        //correct, calculate and display result; otherwise, display Toast error message
         mPlusButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //Plus needs both operands
                 if( !isValidInput( true, true, false )){
                     makeErrorToast();
                 }
@@ -61,6 +72,7 @@ public class MainActivity extends AppCompatActivity {
         mMinusButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //Minus needs both operands
                 if( !isValidInput( true, true, false )){
                     makeErrorToast();
                 }
@@ -75,6 +87,7 @@ public class MainActivity extends AppCompatActivity {
         mMultButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //Multiplication needs both operands
                 if( !isValidInput( true, true, false )){
                     makeErrorToast();
                 }
@@ -89,6 +102,7 @@ public class MainActivity extends AppCompatActivity {
         mDivButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //Division needs both operands and Operand 2 shouln't be 0
                 if( !isValidInput( true, true, true )){
                     makeErrorToast();
                 }
@@ -100,9 +114,11 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
         mPctButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //Percentage only needs first operand
                 if( !isValidInput( true, false, false )){
                     makeErrorToast();
                 }
@@ -117,6 +133,7 @@ public class MainActivity extends AppCompatActivity {
         mSqrtButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //Square Root only needs first operand
                 if( !isValidInput( true, false, false )){
                     makeErrorToast();
                 }
@@ -130,6 +147,18 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    /**
+     *
+     * Checks if the required input strings are valid.  The caller passes in parameters
+     * to specify which values to check.  The assumption, here, is that the user wil
+     * enter two values and press the appropriate button to perform the
+     * desired calculation
+     *
+     * @param NeedOp1		true if the calculation needs a valid operand 1
+     * @param NeedOp2		true if the calculation needss a valid operand 2
+     *
+     * @return			    true if the required inputs are valid, false otherwise
+    **/
     public boolean isValidInput( boolean NeedOp1, boolean NeedOp2, boolean NeedOp2Not0 ){
         String op1 = mOp1.getText().toString();
         String op2 = mOp2.getText().toString();
@@ -144,18 +173,29 @@ public class MainActivity extends AppCompatActivity {
         if( NeedOp2 ){
             if ( op2.length() == 0 )
                 return false;
-            if( Double.parseDouble( op2 ) < 0.0 )
-                return false;
-            if( NeedOp2Not0 ) {
-                if (Double.parseDouble(op2) <= 0.0)
+            try {
+                if (Double.parseDouble(op2) < 0.0)
                     return false;
+                if (NeedOp2Not0) {
+                    if (Double.parseDouble(op2) <= 0.0)
+                        return false;
+                }
+            } catch( NumberFormatException e){
+                Log.e( TAG, "Exception raised when trying to get Doubles from String operands");
             }
         }
 
         return true;
     }
 
+    /**
+     * Displays a Toast Message indicating that some of the operands were incorrect, clears them
+     * and set Focus for entering new operands.
+     */
     public void makeErrorToast(){
-        Toast.makeText( MainActivity.this, "Input Error: Voltage or Current is incorrect", Toast.LENGTH_SHORT).show();
+        mOp1.setText("");
+        mOp2.setText("");
+        Toast.makeText( MainActivity.this, "Input Error: check operands for the desired operation", Toast.LENGTH_SHORT).show();
+        mOp1.requestFocus(); //Set Focus to enter errand operands
     }
 }
